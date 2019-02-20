7b352d01b82c4daa7ef79ac71249aa179ba7d6d7
Ifenna O <okoyeifenna24@gmail.com>
2019-02-20 13:57:36 +0100

    Add tests for DELETE /users/me/token

---
+++ playground/hashing.js
@@ -0,0 +1,41 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err, hash) => {
+		console.log(hash);
+	});
+});
+
+var hashedPassword = '$2a$10$h8jQ/b5PF/fLftrOgEcIOebFnRdUL4OBJddWCWy7587pBpW9XvYAO';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123');
+// console.log(decoded);
+
+// var message = "I am user number 3";
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+// 	id: 4
+// };
+
+// var token = {
+// 	data, 
+// 	hash: SHA256(JSON.stringify((data)).toString())
+// }
---
+++ server/tests/seed/seed.js
@@ -0,0 +1,56 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [{
+	_id: userOneId,
+	email: 'ifenna@example.com',
+	password: 'apassword',
+	tokens: [{
+		access: 'auth',
+		token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+	}]
+}, {
+
+	_id: userTwoId,
+	email: 'enn@example.com',
+	password: 'apassword'
+	
+}];
+
+//add seed data(dummy todos)
+const todos = [{
+	_id: new ObjectID(),
+	text: 'First test todo'
+}, {
+	_id: new ObjectID(),
+	text: 'Second test todo',
+	completed: true,
+	completedAt: 123
+}];
+
+//to empty DB before each request
+const populateTodos = (done) => {
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
+};
+
+const populateUsers = (done) => {
+	User.remove({}).then(() => {
+		//to save HASHED users in other to pass server.test.js
+		//by calling save, we're going to run the middleware.
+		var userOne = new User(users[0]).save();
+		var userTwo = new User(users[1]).save();
+
+		//waits for all save actions to complete
+		return Promise.all([userOne, userTwo])
+	}).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
